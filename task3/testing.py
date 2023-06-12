52577f3ba9df060bb4ffa7ab2f90831c399d7998
Tengyu Song <ts3464@columbia.edu>
2023-06-12 14:05:34 -0400

    Add task 3

---
+++ task3/testing.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import pickle as pkl
+from concurrent.futures import ThreadPoolExecutor
+import os
+from task1.algorithms import matching_pursuit, orthogonal_matching_pursuit
