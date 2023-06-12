d76d4ade534f9240aeb2653569b4ab58a19eb65e
Tengyu Song <ts3464@columbia.edu>
2023-06-12 15:27:07 -0400

    added OMP class in task 3 and small demo

--- task3/algorithms.py
+++ task3/algorithms.py
@@ -1,208 +1,65 @@
 import numpy as np
-from sklearn.linear_model import Lasso
 
-# Matching Pursuit
-def matching_pursuit(s, phi, K):
-    """
-    Perform the Matching Pursuit algorithm
-
-    Args:
-    s (numpy.ndarray): Input signal
-    phi (numpy.ndarray): Dictionary
-    K (int): Number of iterations (sparsity)
-
-    Returns:
-    a (numpy.ndarray): Sparse representation of s
-    indices (list): List of indices of selected atoms
-    coefficients (list): List of coefficients of selected atoms
-    """
-    # Initialize a and r
-    a = np.zeros_like(s)
-    r = s.copy()
-    indices = []
-    coefficients = []
-    # Perform Matching Pursuit
-    for _ in range(K):
-        # Compute inner products
-        inner_products = phi.T @ r
-
-        # Find the index with maximum absolute correlation
-        lambda_k = np.argmax(np.abs(inner_products), axis=0)
-
-        # Save the index
-        indices.append(lambda_k[0])
-
-        # Save the coefficient
-        coefficients.append(inner_products[lambda_k])
-
-        # Update a
-        a += coefficients[-1] * phi[:, lambda_k]
-
-        # Update r
-        r = s - a
-
-    return a, indices, coefficients
-
-
-# Orthogonal Matching Pursuit
-
-def orthogonal_matching_pursuit(s, phi, K):
-    """
-    Perform the Orthogonal Matching Pursuit algorithm
-
-    Args:
-    s (numpy.ndarray): Input signal
-    phi (numpy.ndarray): Dictionary
-    K (int): Number of iterations (sparsity)
-
-    Returns:
-    a (numpy.ndarray): Sparse representation of s
-    indices (list): Indices of the selected atoms
-    coefficients (list): Coefficients of the selected atoms
-    """
-    # Initialize a and r
-    a = np.zeros_like(s)
-    r = s.copy()
-    indices = []
-    coefficients = []
-
-    for _ in range(K):
-        # Compute inner products
-        inner_products = phi.T @ r
-        
-        # Though paper says OMP will not choose the same index twice, it does.
-        inner_products[indices] = np.min(np.abs(inner_products))
-
-        # Find the index with maximum absolute correlation
-        lambda_k = np.argmax(np.abs(inner_products), axis=0)
-        # print(np.max(np.abs(inner_products)))
-        
-        # Save the index
-        indices.append(lambda_k[0])
-        # print(indices)
-
-        # Ordinary Least Squares
-        X = phi[:, indices]
-        
-        try:
-            betas = np.linalg.inv(X.T @ X) @ X.T @ s
-        except np.linalg.LinAlgError:
-            print("Current params:", s, phi, K)
-            print("Singular matrix, stopping the algorithm")
-            break
-
-        # Save the coefficient
-        coefficients = betas
-
-        # Update a
-        a = X @ betas
-
-        # Update r
-        r = s - a
-
-    return a, indices, coefficients
-
-
-# Weak Orthogonal Matching Pursuit
-
-def weak_orthogonal_matching_pursuit(s, phi, alpha):
-    """
-    Perform the Weak Orthogonal Matching Pursuit algorithm
-
-    Args:
-    s (numpy.ndarray): Input signal
-    phi (numpy.ndarray): Dictionary
-    alpha (float): Threshold for stopping the algorithm
-
-    Returns:
-    a (numpy.ndarray): Sparse representation of s
-    indices (list): Indices of the selected atoms
-    coefficients (list): Coefficients of the selected atoms
-    """
-
-    # Initialize a and r
-    a = np.zeros_like(s)
-    r = s.copy()
-    indices = []
-    coefficients = []
-    flag = True
-
-    while flag:
-        # Compute inner products
-        inner_products = phi.T @ r
-
-        # Create a copy of inner_products to find out the largest inner product
-        # Though paper says OMP will not choose the same index twice, it does.
-        inner_products_copy = inner_products.copy()
-        inner_products_copy[indices] = np.min(np.abs(inner_products_copy))
-
-        # Find the index with maximum absolute correlation
-        lambda_k = np.argmax(np.abs(inner_products_copy), axis=0)
-
-        # Save the index
-        indices.append(lambda_k[0])
-
-        # Ordinary Least Squares
-        X = phi[:, indices]
-        betas = np.linalg.inv(X.T @ X) @ X.T @ s
-
-        # Save the coefficient
-        coefficients = betas
-
-        # Update a
-        a = X @ betas
-
-        # Update r
-        r = s - a
-
-        # Check if the largest inner product is greater than alpha times the largest inner product
-        largest_inner_product = np.abs(inner_products[lambda_k][0][0])
-        if largest_inner_product >= alpha * np.max(np.abs(inner_products)):
-            flag = False
-    return a, indices, coefficients
-
-# LASSO
-
-def sparse_LASSO(y, phi, ALPHA):
-    """
-    Perform the sparse LASSO algorithm
-
-    Args:
-    y (numpy.ndarray): Input signal
-    phi (numpy.ndarray): Dictionary
-    alpha (float): Regularization parameter
-
-    Returns:
-    a (numpy.ndarray): Sparse representation of y
-    """
-
-    lasso_signal = Lasso(alpha=ALPHA, fit_intercept=False, max_iter=10000, tol=0.0001)
-    lasso_signal.fit(phi, y)
-    lasso_coefficients = lasso_signal.coef_
-
-    lasso_residual = y - phi @ lasso_signal.coef_
-    lasso_indices = np.nonzero(lasso_coefficients)[0]
-    lasso_coefficients = lasso_coefficients[lasso_indices]
-
-    return lasso_residual, lasso_indices, lasso_coefficients 
-
-
-def bootstrap_sample(y, phi, percentage):
-    '''
-    Perform the bootstrap sample algorithm
-    Args:
-    y (numpy.ndarray): Input signal
-    phi (numpy.ndarray): Dictionary
-    percentage (float): Percentage of the original signal
-    '''
-    # Get the number of samples
-    num_samples = int(percentage * y.shape[0])
+class OMP:
+    def __init__(self, K, random_seed=None):
+        self.K = K
+        self.random_seed = random_seed
     
-    # Get the indices of the samples
-    indices = np.random.choice(y.shape[0], num_samples, replace=True)
+    def fit(self, s, phi):
+
+        """
+        Args:
+        s (numpy.ndarray): Input signal
+        phi (numpy.ndarray): Dictionary
+        """
+
+        self.s = s
+        self.phi = phi
+        self.a = np.zeros_like(self.s)
+        self.c = np.zeros(phi.shape[1])
+        self.r = self.s.copy()
+
+        if self.random_seed is not None:
+            np.random.seed(self.random_seed)
+
+        for i in range(self.K):
+            inner_products = (phi.T @ self.r).flatten()
+            if self.atom_bag_flag:
+                dropping_indices = np.random.choice(phi.shape[1], int(phi.shape[1] * (1 - self.atom_bag_percent)), replace=False)
+                inner_products[dropping_indices] = 0
+            if self.atom_weak_select_flag:
+                top_ind = np.argsort(np.abs(inner_products))[::-1][:int(phi.shape[1] * self.select_atom_percent)]
+                # randomly select one atom
+                lambda_k = np.random.choice(top_ind)
+            else:
+                lambda_k = np.argmax(np.abs(inner_products))
+            
+
+            #Ordinary least squares
+            X = phi[:, self.indices+[lambda_k]]
+
+            ### TODO: 1. Dump Chosen Index 2. Weakly OMP
+            try:
+                betas = np.linalg.inv(X.T @ X) @ X.T @ self.s
+            except:
+                print('Singular matrix encountered in OMP')
+                break
+            #Update indices
+            self.indices.append(lambda_k)
+
+            #Update Coefficients
+            self.c = np.zeros(phi.shape[1])
+            self.c[self.indices] = betas.flatten()
+
+            #Update residual
+            self.r = self.s - X @ betas
+
+            #Update Projection
+            self.a = X @ betas
+        return self.a, self.c
     
-    # Get the samples
-    y_samples = y[indices]
-    phi_samples = phi[indices]
-    
-    return y_samples, phi_samples
+    def score(self, s_test, phi_test):
+        s_pred = phi_test @ self.c
+        pred_mse = np.mean((s_pred - s_test)**2)
+        return pred_mse
+        
--- task3/testing.py
+++ task3/testing.py
@@ -4,6 +4,7 @@ import pickle as pkl
 from concurrent.futures import ThreadPoolExecutor
 import os
 from algorithms import orthogonal_matching_pursuit
+from data_generation import generate_gaussian_noises_dict, generate_sparse_response, generate_perturbed_response
 
 # TODO: Hyperparameters search for omp
 
@@ -13,12 +14,21 @@ from algorithms import orthogonal_matching_pursuit
 ##* X_{ij} \sim N(0, \frac{1}{n}) (The same as N(0,1) them normalize)
 ##* m (sparse level) = 20, 40, 80
 
+##* Noise level = 0.01, 0.05, 0.1, 0.2, 0.5
+
+N = 10000
+d = 1200
+m = 40
+noise_level_lst = [0, 0.01, 0.05, 0.1, 0.2, 0.5]
+
+trial_num = 20
+
 
 ##! Task 1: Given signal, dictionary, sparsity level and noise level, use testset (10% of the whole signal) to find the best K(depth) for omp
 
 
+
 ##! Task 2: Relationship between best K and noise level (std)
-##* Noise level = 0.01, 0.05, 0.1, 0.2, 0.5
 
 
 
