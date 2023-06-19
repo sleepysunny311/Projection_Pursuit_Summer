c1d13be63c7e763cf613fd19369c1453a811bb18
zsrsunny <118696609+sleepysunny311@users.noreply.github.com>
2023-06-19 23:02:56 +0800

    task4 created

---
+++ task4/data_generation.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+def generate_gaussian_noises_dict(N, d, seed=0):
+    np.random.seed(seed)
+    gaussian_noises = np.random.normal(size=(d, N))
+    norms = np.linalg.norm(gaussian_noises, axis=0, keepdims=True)
+    # Create unit-norm vectors
+    unit_vectors = gaussian_noises / norms
+    return unit_vectors
+
+def generate_sparse_response(gaussian_matrix, m, seed=0):
+    np.random.seed(seed)
+    indices = np.random.choice(gaussian_matrix.shape[1], size=m, replace=False)
+    selected_vectors = gaussian_matrix[:, indices]
+    coefficients = np.random.normal(size=(m, 1))  # random coefficients for each selected vector
+    y = selected_vectors @ coefficients
+    return y, indices, coefficients
+
+def generate_perturbed_response(y, noise_level, seed=0):
+    np.random.seed(seed)
+    norm_y = np.linalg.norm(y)
+    noise = np.random.normal(size=y.shape, scale=norm_y * noise_level)
+    y_perturbed = y + noise
+    return y_perturbed
+
+def generate_perturbed_responses(y, noise_levels, seed=0):
+    return [generate_perturbed_response(y, noise_level, seed) for noise_level in noise_levels]
+
+
+
+class DataGeneratorBase:
+
+    # Sub Data Generator Base
+
+    def __init__(self,dictionary_length, dictionary_dimensions, indice_number, noise_level,random_seed,data_path = ""):
+        self.dictionary_length = dictionary_length
+        self.dictionary_dimensions = dictionary_dimensions
+        self.indice_number = indice_number
+        self.noise_level = noise_level
+        self.random_seed = random_seed
+
+        self.dictionary = None
+        self.signal = None
+        self.indices = None
+        self.coefficients = None
+        self.perturbed_signal = None
+        self.coherence_list = None
+        self.coherence = None
+    def generate_dictionary(self):
+        return None
+    
+    def generate_simulated_signal(self):
+        return None
+    
+    def input_noise(self):
+        return None
+    
+    def shuffle(self):
+        np.random.seed(self.random_seed)
+        self.random_seed = np.random.randint(100000000)
+        self.generate_dictionary()
+        self.generate_simulated_signal()
+        self.input_noise()
+        return self.signal, self.dictionary, self.indices, self.coefficients,self.perturbed_signal
+    
+    def get_current_shuffle(self):
+        return self.signal, self.dictionary, self.indices, self.coefficients,self.perturbed_signal
+
+    def retrive_data(self,path = ""):
+        # TODO: retrive data from given data path so you don't have to calcaute the dictionary every goddamn time
+        pass
+    
+    def measure_coherence(self):
+
+        """
+        Measure coherence of the dictionary
+        """
+        temp_coherence =  (self.dictionary).T @ (self.dictionary)
+        self.coherence_list = np.abs(temp_coherence[np.triu_indices(temp_coherence.shape[0], 1)])
+        self.coherence = np.max(self.coherence_list)
+        return self.coherence
+
+
+class GaussianDataGenerator(DataGeneratorBase):
+    def __init__(self, dictionary_length, dictionary_dimensions, indice_number, noise_level, random_seed):
+        super().__init__(dictionary_length, dictionary_dimensions, indice_number, noise_level, random_seed)
+    def generate_dictionary(self):
+        np.random.seed(self.random_seed)
+        gaussian_noises = np.random.normal(size = (self.dictionary_dimensions,self.dictionary_length))
+        norms = np.linalg.norm(gaussian_noises,axis=0, keepdims=True)
+        self.dictionary = gaussian_noises / norms
+    def generate_simulated_signal(self):
+        np.random.seed(self.random_seed)
+        self.indices = np.random.choice(self.dictionary.shape[1], size=self.indice_number, replace=False)
+        self.coefficients = np.random.normal(size=(self.indice_number,1))
+        self.signal = (self.dictionary[:,self.indices]) @ (self.coefficients)
+    def input_noise(self):
+        np.random.seed(self.random_seed)
+        # norm_y = np.linalg.norm(self.signal)
+        norm_beta = np.linalg.norm(self.coefficients)
+        #y' = y/norm beta' = beta/norm
+        # self.signal = self.signal/norm_y
+        #self.coefficients = self.coefficients/norm_y
+        noise = np.random.normal(size=self.signal.shape, scale= self.noise_level*norm_beta)
+        self.perturbed_signal = self.signal + noise
---
+++ task4/demo_visualization.py
@@ -0,0 +1,82 @@
+import os
+import pickle as pkl
+import matplotlib.pyplot as plt
+import glob
+
+def makeplots(res_log, savefig = True):
+    if not os.path.exists('./images'):
+        os.makedirs('./images')
+        
+    # res_log can either be a object name or path to the file
+    if isinstance(res_log, str):
+        path = os.path.join('./memory/', res_log)
+        with open(path, 'rb') as f:
+            res_log = pkl.load(f)
+    
+    # Extract parameters
+    parameters = res_log['parameters']
+    noise_level_best_K = res_log['noise_level_best_K']
+    noise_level_lowest_MSE = res_log['noise_level_lowest_MSE']
+    
+    K_lst = parameters['K_lst']
+    trial_num = res_log['parameters']['trial_num']
+    print('Parameters: ', parameters)
+    
+    # Get folder name for saving images
+    imgfolderPath = str(parameters['N']) + '_' + str(parameters['d']) + '_' + str(parameters['m']) + '_' + str(parameters['trial_num']) + '_' + str(parameters['cv_num'])
+    if savefig:
+        if not os.path.exists('./images/' + imgfolderPath):
+            os.makedirs('./images/' + imgfolderPath)
+    
+    # Generate plots
+    ## First plot: Iteration number vs. CV prediction error for each noise level
+    noise_level_lst = parameters['noise_level_lst']
+    tmp = []
+    for i in range(len(res_log['log'])):
+        tmp.append([res_log['log'][i]['noise_level'], res_log['log'][i]['trial'], res_log['log'][i]['cv_error_lst']])
+    fig, ax = plt.subplots()
+    # Plot the cv error for each K for each noise level
+    for i in range(len(tmp)):    
+        if i % trial_num == 0:
+            label = tmp[i][0]
+        else:
+            label = ""
+        # Assign color to different noise level
+        plt.plot(K_lst, tmp[i][2], label = label, color = plt.cm.Set2_r(i // trial_num))
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+    ax.get_legend().set_title("Noise std")
+
+    
+    plt.xlabel("Iteration number")
+    plt.ylabel("CV prediction error")
+    plt.title("CV prediction error for each noise level with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
+    if savefig:
+        plt.savefig('./images/' + imgfolderPath + '/cv_error.png', dpi=200, bbox_inches='tight')
+    plt.show()
+
+    ## Second plot: Noise std vs. best iteration number
+    plt.plot(noise_level_lst, noise_level_best_K)
+    plt.title("Best iteration number for each noise level with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
+    plt.xlabel("Noise std")
+    plt.ylabel("Best iteration number")
+    if savefig:
+        plt.savefig('./images/' + imgfolderPath + '/noise_vs_bestK.png', dpi=200, bbox_inches='tight')
+    plt.show()
+    
+    ## Third plot: Noise std vs. CV prediction error for best iteration number
+    plt.plot(noise_level_lst, noise_level_lowest_MSE)
+    plt.xlabel("Noise std")
+    plt.ylabel("CV prediction error for best K")
+    plt.title("CV prediction error for best iteration number for each noise level with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
+    if savefig:
+        plt.savefig('./images/' + imgfolderPath + '/noise_vs_bestMSE.png', dpi=200, bbox_inches='tight')
+    plt.show()
+
+# Extracing All information
+pkl_files = glob.glob("memory/*.pkl")
+parameter_dict_list = []
+for i in range(len(pkl_files)):
+    with open(pkl_files[i], 'rb') as f:
+        res_log = pkl.load(f)
+    makeplots(res_log, savefig = True)
