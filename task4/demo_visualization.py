b4cc3da6dcffa131a281f52db72ebd106ea5897d
Tengyu Song <ts3464@columbia.edu>
2023-06-20 15:39:01 -0400

    improve makeplot function, now can plot cv prediction error for each noise level separately

--- task4/demo_visualization.py
+++ task4/demo_visualization.py
@@ -3,7 +3,7 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 import glob
 
-def makeplots(res_log, savefig = True):
+def makeplots(res_log, sep=False, savefig = True):
     if not os.path.exists('./images'):
         os.makedirs('./images')
         
@@ -31,29 +31,46 @@ def makeplots(res_log, savefig = True):
     # Generate plots
     ## First plot: Iteration number vs. CV prediction error for each noise level
     noise_level_lst = parameters['noise_level_lst']
-    tmp = []
-    for i in range(len(res_log['log'])):
-        tmp.append([res_log['log'][i]['noise_level'], res_log['log'][i]['trial'], res_log['log'][i]['cv_error_lst']])
-    fig, ax = plt.subplots()
-    # Plot the cv error for each K for each noise level
-    for i in range(len(tmp)):    
-        if i % trial_num == 0:
-            label = tmp[i][0]
-        else:
-            label = ""
-        # Assign color to different noise level
-        plt.plot(K_lst, tmp[i][2], label = label, color = plt.cm.Set2_r(i // trial_num))
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    if sep:
+        if not os.path.exists('./images/' + imgfolderPath + '/noise_sep'):
+            os.makedirs('./images/' + imgfolderPath + '/noise_sep')
+        # Plot the cv error for each K for each noise level in separate plots
+        # Each noise level has trial_num trials and we put them in the same plot
+        for i in range(len(res_log['log'])):
+            fig, ax = plt.subplots()
+            for j in range(trial_num):
+                plt.plot(K_lst, res_log['log'][i]['cv_error_lst'][j], label = "Trial " + str(j + 1))
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+            plt.xlabel("Iteration number")
+            plt.ylabel("CV prediction error")
+            plt.title("CV prediction error for noise level " + str(res_log['log'][i]['noise_level']) + " with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
+            if savefig:
+                plt.savefig('./images/' + imgfolderPath + '/noise_sep/' + str(res_log['log'][i]['noise_level']) + '.png', dpi=200, bbox_inches='tight')
+            plt.show()
+    else:
+        tmp = []
+        for i in range(len(res_log['log'])):
+            tmp.append([res_log['log'][i]['noise_level'], res_log['log'][i]['trial'], res_log['log'][i]['cv_error_lst']])
+        fig, ax = plt.subplots()
+        # Plot the cv error for each K for each noise level
+        for i in range(len(tmp)):    
+            if i % trial_num == 0:
+                label = tmp[i][0]
+            else:
+                label = ""
+            # Assign color to different noise level
+            plt.plot(K_lst, tmp[i][2], label = label, color = plt.cm.Set2_r(i // trial_num))
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
-    ax.get_legend().set_title("Noise std")
+        ax.get_legend().set_title("Noise std")
 
-    
-    plt.xlabel("Iteration number")
-    plt.ylabel("CV prediction error")
-    plt.title("CV prediction error for each noise level with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
-    if savefig:
-        plt.savefig('./images/' + imgfolderPath + '/cv_error.png', dpi=200, bbox_inches='tight')
-    plt.show()
+        
+        plt.xlabel("Iteration number")
+        plt.ylabel("CV prediction error")
+        plt.title("CV prediction error for each noise level with m = " + str(parameters['m']) + " and d = " + str(parameters['d']))
+        if savefig:
+            plt.savefig('./images/' + imgfolderPath + '/cv_error.png', dpi=200, bbox_inches='tight')
+        plt.show()
 
     ## Second plot: Noise std vs. best iteration number
     plt.plot(noise_level_lst, noise_level_best_K)
@@ -73,10 +90,10 @@ def makeplots(res_log, savefig = True):
         plt.savefig('./images/' + imgfolderPath + '/noise_vs_bestMSE.png', dpi=200, bbox_inches='tight')
     plt.show()
 
-# Extracing All information
-pkl_files = glob.glob("memory/*.pkl")
-parameter_dict_list = []
-for i in range(len(pkl_files)):
-    with open(pkl_files[i], 'rb') as f:
-        res_log = pkl.load(f)
-    makeplots(res_log, savefig = True)
+# # Extracing All information
+# pkl_files = glob.glob("memory/*.pkl")
+# parameter_dict_list = []
+# for i in range(len(pkl_files)):
+#     with open(pkl_files[i], 'rb') as f:
+#         res_log = pkl.load(f)
+#     makeplots(res_log, savefig = True)
