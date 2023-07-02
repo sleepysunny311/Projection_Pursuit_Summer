d873f1b8b2678e888524f396f92540493ec49607
zsrsunny <118696609+sleepysunny311@users.noreply.github.com>
2023-07-02 11:41:32 -0400

    Revised OMP inherenting AtomBaggingBase

--- task5/algorithms.py
+++ task5/algorithms.py
@@ -1,90 +1,6 @@
 import numpy as np
 from sklearn.base import BaseEstimator
 
-
-class OMP:
-    def __init__(
-        self, K, select_atom_percent=0, random_seed=None, ignore_warning=False
-    ):
-        self.K = K
-        self.random_seed = random_seed
-        self.select_atom_percent = select_atom_percent
-        if select_atom_percent == 0:
-            self.atom_weak_select_flag = False
-
-        self.indices = []
-        self.coefficients = None
-        self.ignore_warning = ignore_warning
-
-    def fit(self, phi, s):
-        """
-        Args:
-        s (numpy.ndarray): Input signal
-        phi (numpy.ndarray): Dictionary
-        """
-
-        self.s = s
-        self.phi = phi
-        self.a = np.zeros_like(self.s)
-        self.coefficients = np.zeros(phi.shape[1])
-        self.r = self.s.copy()
-
-        if self.random_seed is not None:
-            np.random.seed(self.random_seed)
-
-        for i in range(self.K):
-            inner_products = (phi.T @ self.r).flatten()
-            # so that we will not select the same atom
-            inner_products[self.indices] = 0
-            if self.atom_weak_select_flag:
-                top_ind = np.argsort(np.abs(inner_products))[::-1][
-                    : int(phi.shape[1] * self.select_atom_percent)
-                ]
-                # randomly select one atom
-                lambda_k = np.random.choice(top_ind)
-            else:
-                lambda_k = np.argmax(np.abs(inner_products))
-
-            # Ordinary least squares
-            X = phi[:, self.indices + [lambda_k]]
-
-            try:
-                betas = np.linalg.inv(X.T @ X) @ X.T @ self.s
-            except:
-                if not self.ignore_warning:
-                    print("Singular matrix encountered in OMP")
-                break
-
-            # Update indices
-            self.indices.append(lambda_k)
-
-            # Update Coefficients
-            self.coefficients = np.zeros(phi.shape[1])
-            self.coefficients[self.indices] = betas.flatten()
-
-            # Update residual
-            self.r = self.s - X @ betas
-
-            # Update Projection
-            self.a = X @ betas
-        return self.a, self.coefficients
-
-    def score(self, phi_test, s_test):
-        s_pred = self.predict(phi_test)
-        pred_mse = np.mean((s_pred - s_test) ** 2)
-        return pred_mse
-
-    def predict(self, phi_test):
-        """
-        Args:
-        phi_test (numpy.ndarray): Test data
-
-        Returns:
-        numpy.ndarray: Predicted output
-        """
-        return phi_test @ self.coefficients
-
-
 # This file contains classes for different pursuit algorithms
 
 
@@ -227,6 +143,74 @@ class AtomBaggingBase(BaseEstimator):
 
     def input_coefficients(self, coefficients):
         self.coefficients = coefficients
+
+
+class OMP(AtomBaggingBase):
+    def __init__(
+        self, K, select_atom_percent=0, random_seed=None, ignore_warning=False
+    ):
+        self.K = K
+        self.random_seed = random_seed
+        self.select_atom_percent = select_atom_percent
+        if select_atom_percent == 0:
+            self.atom_weak_select_flag = False
+
+        self.indices = []
+        self.coefficients = None
+        self.ignore_warning = ignore_warning
+
+    def fit(self, phi, s):
+        """
+        Args:
+        s (numpy.ndarray): Input signal
+        phi (numpy.ndarray): Dictionary
+        """
+
+        self.s = s
+        self.phi = phi
+        self.a = np.zeros_like(self.s)
+        self.coefficients = np.zeros(phi.shape[1])
+        self.r = self.s.copy()
+
+        if self.random_seed is not None:
+            np.random.seed(self.random_seed)
+
+        for i in range(self.K):
+            inner_products = (phi.T @ self.r).flatten()
+            # so that we will not select the same atom
+            inner_products[self.indices] = 0
+            if self.atom_weak_select_flag:
+                top_ind = np.argsort(np.abs(inner_products))[::-1][
+                    : int(phi.shape[1] * self.select_atom_percent)
+                ]
+                # randomly select one atom
+                lambda_k = np.random.choice(top_ind)
+            else:
+                lambda_k = np.argmax(np.abs(inner_products))
+
+            # Ordinary least squares
+            X = phi[:, self.indices + [lambda_k]]
+
+            try:
+                betas = np.linalg.inv(X.T @ X) @ X.T @ self.s
+            except:
+                if not self.ignore_warning:
+                    print("Singular matrix encountered in OMP")
+                break
+
+            # Update indices
+            self.indices.append(lambda_k)
+
+            # Update Coefficients
+            self.coefficients = np.zeros(phi.shape[1])
+            self.coefficients[self.indices] = betas.flatten()
+
+            # Update residual
+            self.r = self.s - X @ betas
+
+            # Update Projection
+            self.a = X @ betas
+        return self.a, self.coefficients
 
 
 class AtomBaggingMatchingPursuit(AtomBaggingBase):
