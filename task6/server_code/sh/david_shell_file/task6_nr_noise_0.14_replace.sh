7ec0d1e477ea4cfd81aeee09b60249a36a2f6ec6
zsrsunny <118696609+sleepysunny311@users.noreply.github.com>
2023-07-10 23:15:01 -0400

    yaml & shell file generator

---
+++ task6/server_code/sh/david_shell_file/task6_nr_noise_0.14_replace.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --account=stats
+#SBATCH --job-name=task6_r_part2
+#SBATCH -c 16
+#SBATCH -t 5-00:00
+#SBATCH -C mem192
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=sz3091@columbia.edu
+ 
+module load anaconda
+#Command to execute Python program
+python BOMP_testing.py --config ./configs/no_replacement/task6_nr_noise_0.14_replace.yaml
+#End of script
