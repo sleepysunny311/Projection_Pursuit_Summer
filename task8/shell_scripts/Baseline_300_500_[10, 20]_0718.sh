da3be85e395c25f0fe1adf1883e8f16b9102835b
zsrsunny <118696609+sleepysunny311@users.noreply.github.com>
2023-07-18 17:00:11 -0400

    Reduced the problem size

--- task8/shell_scripts/Baseline_300_500_[10, 20]_0718.sh
+++ task8/shell_scripts/Baseline_300_500_[10, 20]_0718.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --account=stats
-#SBATCH --job-name=BOMP_600_1000_[10, 20]_0718
+#SBATCH --job-name=BOMP_300_500_[10, 20]_0718
 #SBATCH -c 16
 #SBATCH -t 5-00:00
 #SBATCH -C mem192
@@ -9,5 +9,5 @@
 
 module load anaconda
 #Command to execute Python program
-python OMP_testing.py --config-name BOMP_600_1000_[10, 20]_r_0718.yaml --config-path ./configs
+python OMP_testing.py --config-name BOMP_300_500_[10, 20]_nr_0718.yaml --config-path ./configs
 #End of script
